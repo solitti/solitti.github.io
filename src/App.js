d144855a129fe5870dbec57bfc6e6967670ca955
Solitti Solutions <solittisolutions@gmail.com>
2020-02-23 16:24:55 -0300

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import ReactGA from "react-ga";
 import "./App.css";
 
 import Maintenance from "./pages/Maintenance";
 
+function initializeReactGA() {
+  ReactGA.initialize("UA-158961920-1");
+  ReactGA.pageview("/maintenancepage");
+}
+
 function App() {
+  initializeReactGA();
+
   return <Maintenance />;
 }
 
