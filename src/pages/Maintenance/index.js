62d4b196e68bfeb2e82646d2260aa8a9840bda27
Solitti Solutions <solittisolutions@gmail.com>
2021-07-06 21:31:21 -0300

    refac: remove phone number

--- src/pages/Maintenance/index.js
+++ src/pages/Maintenance/index.js
@@ -1,26 +1,23 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-import { Container } from "./styles";
+import { Container } from './styles';
 
 export default function Maintenance() {
   return (
     <Container>
-      <div className="box-logo">
-        <span className="logo">SOLITTI</span>
-        <span className="nature">SOLUTIONS</span>
-      </div>
+      <a href="https://solitti.com.br">
+        <div className="box-logo">
+          <span className="logo">SOLITTI</span>
+          <span className="nature">SOLUTIONS</span>
+        </div>
+      </a>
       <div className="box-contact">
         <span>
-          <a href="https://wa.me/5531993841675" target="blank">
-            <FontAwesomeIcon icon={faWhatsapp} /> 31 993841675
-          </a>
-        </span>
-        <span>
-          <a href="mailto:contato@solitti.com.br">
-            <FontAwesomeIcon icon={faEnvelope} /> contato@solitti.com.br
+          <a href="mailto:hi@solitti.com.br">
+            <FontAwesomeIcon icon={faEnvelope} /> hi@solitti.com.br
           </a>
         </span>
       </div>
